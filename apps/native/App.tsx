5b26b87fe09db2275434538152354dea0ac84a62
hjkim0905 <eax9952@gmail.com>
2026-05-02 16:28:19 +0900

    feat: implement WebView settings

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WebView source={{ uri: 'http://localhost:3000' }} style={styles.container} />;
 }
 
 const styles = StyleSheet.create({

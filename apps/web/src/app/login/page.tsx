91f197f96dc5f46922f0e90f769489dc50b4fc9f
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-05 20:35:04 +0900

    feat: request kakao login oauth

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function page() {
+  const OAUTH_REQUEST_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}`;
+
+  return (
+    <>
+      <Link href={`${OAUTH_REQUEST_URL}`}>카카오로 시작하기</Link>
+    </>
+  );
+}

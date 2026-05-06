1cc3ad2aa0f88a21d3783983524fb20210ff3f0e
hjkim0905 <eax9952@gmail.com>
2026-05-06 23:12:18 +0900

    chore: migrate native app config to app.config.js and add build scripts

---
+++ apps/native/app.config.js
@@ -0,0 +1,46 @@
+export default {
+  expo: {
+    name: 'native',
+    slug: 'native',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.company.tolli',
+    },
+    android: {
+      package: 'com.company.tolli',
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    scheme: 'tolli',
+    plugins: [
+      [
+        '@react-native-google-signin/google-signin',
+        {
+          iosUrlScheme: 'com.googleusercontent.apps.453019507405-qq1d999d55r3onrsdoednvtmjggfq6vf',
+          googleServicesFile: './android/app/google-services.json',
+        },
+      ],
+    ],
+    extra: {
+      googleWebClientId: process.env.GOOGLE_WEB_CLIENT_ID,
+      googleIosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+    },
+  },
+};

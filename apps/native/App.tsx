3e2a56fdefb90aefbe551d208800a858da196e96
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-06 23:51:54 +0900

    feat: add URL interceptor for Kakao deep linking

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,17 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import { Linking, StyleSheet, Alert } from "react-native";
+import { WebView } from "react-native-webview";
+import { ShouldStartLoadRequest } from "react-native-webview/lib/WebViewTypes";
+import { Platform } from "react-native";
+
+const parseIntentUrl = (url: string) => {
+  if (!url.startsWith("intent:")) return url;
+  const schemeMatch = url.match(/scheme=([^;]+)/);
+  if (schemeMatch && schemeMatch[1]) {
+    const scheme = schemeMatch[1];
+    const path = url.replace("intent://", "").split("#")[0];
+    return `${scheme}://${path}`;
+  }
+  return url;
+};
 
 export default function App() {
-  return <WebView source={{ uri: 'http://localhost:3000' }} style={styles.container} />;
+  const onShouldStartLoadWithRequest = (event: ShouldStartLoadRequest) => {
+    if (event.url.startsWith("http") || event.url.startsWith("https")) {
+      return true;
+    } else if (Platform.OS === "android") {
+      const kakaoAuthUrl = parseIntentUrl(event.url);
+
+      const loginWithKakao = async () => {
+        const supported = await Linking.canOpenURL(kakaoAuthUrl);
+        if (supported) {
+          await Linking.openURL(kakaoAuthUrl);
+        } else {
+          Alert.alert("오류", "카카오톡을 열 수 없습니다.");
+        }
+      };
+      loginWithKakao();
+      return false;
+    } else if (Platform.OS === "ios") {
+      const kakaoAuthUrl = event.url;
+
+      const loginWithKakao = async () => {
+        const supported = await Linking.canOpenURL(kakaoAuthUrl);
+        if (supported) {
+          await Linking.openURL(kakaoAuthUrl);
+        } else {
+          Alert.alert("오류", "카카오톡을 열 수 없습니다.");
+        }
+      };
+      loginWithKakao();
+      return false;
+    }
+    return true;
+  };
+
+  return (
+    <WebView
+      source={{ uri: "http://192.168.35.166:3000" }}
+      style={styles.container}
+      onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

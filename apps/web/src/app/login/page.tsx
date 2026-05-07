2539a495072f0ee6c2cc7f3ae27ce0a1f49c1dd2
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-07 21:48:12 +0900

    feat: send message from web to app

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,11 +1,25 @@
-import Link from "next/link";
+import { useIsReactNativeWebview } from "../hooks/useIsReactNativeWebview ";
 
 export default function page() {
-  const OAUTH_REQUEST_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+  const isWebView = useIsReactNativeWebview();
+
+  const handleKakaoLogin = () => {
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+
+    if (isWebView) {
+      const message = JSON.stringify({
+        type: "KAKAO_LOGIN",
+        url: KAKAO_AUTH_URL,
+      });
+      window?.ReactNativeWebView.postMessage(message);
+    } else {
+      window.location.href = KAKAO_AUTH_URL;
+    }
+  };
 
   return (
     <>
-      <Link href={`${OAUTH_REQUEST_URL}`}>카카오로 시작하기</Link>
+      <button onClick={handleKakaoLogin}>카카오톡으로 시작하기</button>
     </>
   );
 }

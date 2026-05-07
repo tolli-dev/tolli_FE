ab4b10b16865fc641d0e315a3c6430b8155d41e3
hjkim0905 <eax9952@gmail.com>
2026-05-07 14:01:10 +0900

    chore: change name of the app

--- apps/native/app.config.js
+++ apps/native/app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
-    name: 'native',
-    slug: 'native',
+    name: 'Tolli',
+    slug: 'Tolli',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',

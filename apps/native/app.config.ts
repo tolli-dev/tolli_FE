13b30ac6086948944044eff2551b0370ee47e8dd
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-07 13:00:57 +0900

    chore: modify android/ios plugin

---
+++ apps/native/app.config.ts
@@ -0,0 +1,36 @@
+import "tsx/cjs";
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "native",
+  slug: "native",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.tolli.native",
+  },
+  android: {
+    package: "com.tolli.native",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+
+  plugins: ["./plugins/withPlugin.ts"],
+});

fad8ac05ab539c84604d81cfd69436df70b13cad
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-14 17:27:54 +0900

    feat: make setAlarm page

---
+++ apps/web/src/app/onBoarding/afterLogin/setAlarm/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import TimeTolly from "../../../../../public/images/onBoarding/timeTolli.svg";
+
+export default function Page() {
+  return (
+    <section className="flex flex-col w-full min-h-[100dvh] justify-between items-center px-[2.688rem] py-[5.313rem]">
+      <div className="flex flex-col items-center w-full mt-[clamp(1rem,4dvh,10.25rem)]">
+        <div className="flex flex-col items-center justify-center w-full gap-[0.563rem]">
+          <div className="flex flex-col items-center justify-center gap-[0.125rem]">
+            <h1 className="text-h1 text-primary-50">말씀이 찾아오는 시간,</h1>
+            <h1 className="text-h1 text-primary-50">톨리가 알려드릴게요!</h1>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-[0.125rem]">
+            <h2 className="text-h2 text-surface-200">
+              매일 정해진 시간에 조용히 알려드려요.
+            </h2>
+            <h2 className="text-h2 text-surface-200">
+              tolli가 배고프면 찾아가요.
+            </h2>
+          </div>
+        </div>
+
+        <div className="relative w-full max-w-[16.125rem] aspect-square">
+          <Image
+            src={TimeTolly}
+            fill
+            alt="timeTolli"
+            className="object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center w-full gap-[0.563rem]">
+        <button
+          type="button"
+          className="
+              w-full max-w-[19.688rem] h-[3rem] text-btn-lg 
+              text-bg bg-primary-75 rounded-[1.25rem]
+            "
+        >
+          알림 허용할래요
+        </button>
+        <div className="flex items-center">
+          <p className="flex w-full text-primary-75 text-no-alarm underline decoration-primary-75">
+            나중에 설정할게요
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}

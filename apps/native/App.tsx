e947aa2100974d06fdb94b563cc4406c0fa987fa
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-15 15:25:17 +0900

    refactor: modify android kakao login logic (need to update new logic)

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -19,6 +19,7 @@ GoogleSignin.configure({
 
 export default function App() {
   const webviewRef = useRef<WebViewType>(null);
+  const IP_URL = "http://192.168.1.177:3000";
 
   const postToken = (type: string, token: string) => {
     webviewRef.current?.postMessage(JSON.stringify({ type, token }));
@@ -40,16 +41,16 @@ export default function App() {
         if (idToken) postToken("APPLE_TOKEN", idToken);
       }
 
-      if (data.type === "KAKAO_LOGIN") {
-        const returnUrl = Linking.createURL("");
-        const result = await WebBrowser.openAuthSessionAsync(
-          data.url,
-          returnUrl,
-        );
-        if (result.type === "success") {
-          webviewRef.current?.reload();
-        }
-      }
+      // if (data.type === "KAKAO_LOGIN") {
+      //   const returnUrl = Linking.createURL("");
+      //   const result = await WebBrowser.openAuthSessionAsync(
+      //     data.url,
+      //     returnUrl,
+      //   );
+      //   if (result.type === "success") {
+      //     webviewRef.current?.reload();
+      //   }
+      // }
     } catch (error: any) {
       if (
         error.code === "SIGN_IN_CANCELLED" ||
@@ -63,9 +64,30 @@ export default function App() {
   return (
     <WebView
       ref={webviewRef}
-      source={{ uri: "http://172.25.32.1:3000" }}
+      source={{ uri: "http://192.168.1.177:3000" }}
       style={styles.container}
       onMessage={handleMessage}
+      onShouldStartLoadWithRequest={(request) => {
+        const { url } = request;
+
+        // http, https 등 일반적인 웹 이동은 허용
+        if (
+          url.startsWith("http://") ||
+          url.startsWith("https://") ||
+          url.startsWith("about:blank")
+        ) {
+          return true;
+        }
+
+        // intent:// 나 kakaotalk:// 같은 외부 앱 스킴이 들어오면 외부 앱 열기 시도
+        Linking.openURL(url).catch(() => {
+          console.log(
+            "해당 앱(카카오톡)이 설치되어 있지 않거나 열 수 없습니다.",
+          );
+        });
+
+        return false; // 웹뷰 내부에서는 알 수 없는 스킴 이동 방지
+      }}
       scalesPageToFit={false}
       bounces={false}
       overScrollMode="never"
--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -10,20 +10,20 @@ import { useIsReactNativeWebview } from "../hooks/useIsReactNativeWebview ";
 
 export default function LoginPage() {
   const router = useRouter();
-  const isWebView = useIsReactNativeWebview();
+  // const isWebView = useIsReactNativeWebview();
 
   const handleKakaoLogin = () => {
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
-
-    if (isWebView) {
-      const message = JSON.stringify({
-        type: "KAKAO_LOGIN",
-        url: KAKAO_AUTH_URL,
-      });
-      window?.ReactNativeWebView?.postMessage(message);
-    } else {
-      window.location.href = KAKAO_AUTH_URL;
-    }
+    window.location.href = KAKAO_AUTH_URL;
+    // if (isWebView) {
+    //   const message = JSON.stringify({
+    //     type: "KAKAO_LOGIN",
+    //     url: KAKAO_AUTH_URL,
+    //   });
+    //   window?.ReactNativeWebView?.postMessage(message);
+    // } else {
+    //   window.location.href = KAKAO_AUTH_URL;
+    // }
   };
 
   const requestGoogleLogin = () => {

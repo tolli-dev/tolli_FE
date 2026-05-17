7a7ca3c8d95620f2adbd3003e21288b5aa9b0ee7
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-18 01:17:24 +0900

    refactor: delete unused file and description

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -9,8 +9,6 @@ import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import Constants from "expo-constants";
 import { signInWithGoogle } from "./auth/googleSignIn";
 import { signInWithApple } from "./auth/appleSignIn";
-import * as Linking from "expo-linking";
-import * as WebBrowser from "expo-web-browser";
 import { IP_URL } from "../web/src/constants/url";
 import { KakaoOAuthToken, login } from "@react-native-seoul/kakao-login";
 
@@ -46,17 +44,6 @@ export default function App() {
         const idToken: KakaoOAuthToken = await login();
         if (idToken) postToken("KAKAO_TOKEN", idToken.accessToken);
       }
-
-      // if (data.type === "KAKAO_LOGIN") {
-      //   const returnUrl = Linking.createURL("");
-      //   const result = await WebBrowser.openAuthSessionAsync(
-      //     data.url,
-      //     returnUrl,
-      //   );
-      //   if (result.type === "success") {
-      //     webviewRef.current?.reload();
-      //   }
-      // }
     } catch (error: any) {
       if (
         error.code === "SIGN_IN_CANCELLED" ||
@@ -74,27 +61,6 @@ export default function App() {
       style={styles.container}
       onMessage={handleMessage}
       contentInsetAdjustmentBehavior="never"
-      // onShouldStartLoadWithRequest={(request) => {
-      //   const { url } = request;
-
-      //   // http, https 등 일반적인 웹 이동은 허용
-      //   if (
-      //     url.startsWith("http://") ||
-      //     url.startsWith("https://") ||
-      //     url.startsWith("about:blank")
-      //   ) {
-      //     return true;
-      //   }
-
-      //   // intent:// 나 kakaotalk:// 같은 외부 앱 스킴이 들어오면 외부 앱 열기 시도
-      //   Linking.openURL(url).catch(() => {
-      //     console.log(
-      //       "해당 앱(카카오톡)이 설치되어 있지 않거나 열 수 없습니다.",
-      //     );
-      //   });
-
-      //   return false; // 웹뷰 내부에서는 알 수 없는 스킴 이동 방지
-      // }}
       scalesPageToFit={false}
       bounces={false}
       overScrollMode="never"

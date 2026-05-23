919e64dd48191876e0b70ea4d748bcd4b8234322
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-23 22:12:01 +0900

    feat: link next step when user click screen

--- apps/web/src/app/study/_components/ReadVerse.tsx
+++ apps/web/src/app/study/_components/ReadVerse.tsx
@@ -1,23 +1,26 @@
 import { Verse } from "./types";
+import Link from "next/link";
 
 export default function ReadVerse({ verse }: { verse: Verse }) {
   return (
-    <section className="flex flex-col flex-1">
-      <div className="flex flex-col mt-[clamp(5rem,20vh,10rem)] justify-center gap-[clamp(2rem,8vw,4.25rem)] px-[clamp(1.5rem,9vw,4.25rem)]">
-        <p className="text-center text-[clamp(0.875rem,4vw,1rem)] font-medium leading-5 tracking-[0.03em] text-[#CCB5F0]">
-          {verse.reference}
-        </p>
-        <div className="flex flex-wrap justify-center">
-          {verse.words.map((value) => (
-            <span
-              key={value.index}
-              className="px-1 text-[clamp(1.125rem,5vw,1.5rem)] text-[#D7D2DF] leading-[clamp(2rem,8vw,2.875rem)]"
-            >
-              {value.text}
-            </span>
-          ))}
+    <Link className="w-full h-dvh" href={`/study/${verse.id}/1`}>
+      <section className="flex flex-col flex-1">
+        <div className="flex flex-col mt-[clamp(5rem,20vh,10rem)] justify-center gap-[clamp(2rem,8vw,4.25rem)] px-[clamp(1.5rem,9vw,4.25rem)]">
+          <p className="text-center text-[clamp(0.875rem,4vw,1rem)] font-medium leading-5 tracking-[0.03em] text-[#CCB5F0]">
+            {verse.reference}
+          </p>
+          <div className="flex flex-wrap justify-center">
+            {verse.words.map((value) => (
+              <span
+                key={value.index}
+                className="px-1 text-[clamp(1.125rem,5vw,1.5rem)] text-[#D7D2DF] leading-[clamp(2rem,8vw,2.875rem)]"
+              >
+                {value.text}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Link>
   );
 }

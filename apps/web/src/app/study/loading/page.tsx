a819880e4ff4249663e73671655be6c5f61f0913
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-24 12:55:40 +0900

    feat: add autuo router.replace when time is out

--- apps/web/src/app/study/loading/page.tsx
+++ apps/web/src/app/study/loading/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import ReadingBookTolli from "../../../../public/images/readingBookTolli.svg";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Loading() {
+  const router = useRouter();
+
+  // 백엔드에서 데이터를 다 불러오면 step 미션 페이지로 이동하기
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/study/30/0");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div className="loading-screen flex flex-col h-full ">
       <div className="flex flex-col flex-1 w-full">

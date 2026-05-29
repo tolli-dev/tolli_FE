a933df125d905693430c5e1517aa347c5dc257d3
hjkim0905 <eax9952@gmail.com>
2026-05-29 22:47:07 +0900

    chore: add verses seed data

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -1,8 +1,107 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
+import { queryRef, executeQuery, validateArgsWithOptions, mutationRef, executeMutation, validateArgs } from 'firebase/data-connect';
 
 export const connectorConfig = {
   connector: 'default',
-  service: 'tolli_FE',
-  location: 'us-central1'
+  service: 'tolli-fe',
+  location: 'asia-northeast3'
 };
+export const createUserRef = (dcOrVars, vars) => {
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
+  dcInstance._useGeneratedSdk();
+  return mutationRef(dcInstance, 'CreateUser', inputVars);
+}
+createUserRef.operationName = 'CreateUser';
+
+export function createUser(dcOrVars, vars) {
+  const { dc: dcInstance, vars: inputVars } = validateArgs(connectorConfig, dcOrVars, vars, true);
+  return executeMutation(createUserRef(dcInstance, inputVars));
+}
+
+export const createStudyCompletionRef = (dcOrVars, vars) => {
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
+  dcInstance._useGeneratedSdk();
+  return mutationRef(dcInstance, 'CreateStudyCompletion', inputVars);
+}
+createStudyCompletionRef.operationName = 'CreateStudyCompletion';
+
+export function createStudyCompletion(dcOrVars, vars) {
+  const { dc: dcInstance, vars: inputVars } = validateArgs(connectorConfig, dcOrVars, vars, true);
+  return executeMutation(createStudyCompletionRef(dcInstance, inputVars));
+}
+
+export const addBookmarkRef = (dcOrVars, vars) => {
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
+  dcInstance._useGeneratedSdk();
+  return mutationRef(dcInstance, 'AddBookmark', inputVars);
+}
+addBookmarkRef.operationName = 'AddBookmark';
+
+export function addBookmark(dcOrVars, vars) {
+  const { dc: dcInstance, vars: inputVars } = validateArgs(connectorConfig, dcOrVars, vars, true);
+  return executeMutation(addBookmarkRef(dcInstance, inputVars));
+}
+
+export const deleteBookmarkRef = (dcOrVars, vars) => {
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
+  dcInstance._useGeneratedSdk();
+  return mutationRef(dcInstance, 'DeleteBookmark', inputVars);
+}
+deleteBookmarkRef.operationName = 'DeleteBookmark';
+
+export function deleteBookmark(dcOrVars, vars) {
+  const { dc: dcInstance, vars: inputVars } = validateArgs(connectorConfig, dcOrVars, vars, true);
+  return executeMutation(deleteBookmarkRef(dcInstance, inputVars));
+}
+
+export const getVerseRef = (dcOrVars, vars) => {
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
+  dcInstance._useGeneratedSdk();
+  return queryRef(dcInstance, 'GetVerse', inputVars);
+}
+getVerseRef.operationName = 'GetVerse';
+
+export function getVerse(dcOrVars, varsOrOptions, options) {
+  
+  const { dc: dcInstance, vars: inputVars, options: inputOpts } = validateArgsWithOptions(connectorConfig, dcOrVars, varsOrOptions, options, true, true);
+  return executeQuery(getVerseRef(dcInstance, inputVars), inputOpts && { fetchPolicy: inputOpts.fetchPolicy });
+}
+
+export const getMyCurrentVerseRef = (dc) => {
+  const { dc: dcInstance} = validateArgs(connectorConfig, dc, undefined);
+  dcInstance._useGeneratedSdk();
+  return queryRef(dcInstance, 'GetMyCurrentVerse');
+}
+getMyCurrentVerseRef.operationName = 'GetMyCurrentVerse';
+
+export function getMyCurrentVerse(dcOrOptions, options) {
+  
+  const { dc: dcInstance, vars: inputVars, options: inputOpts } = validateArgsWithOptions(connectorConfig, dcOrOptions, options, undefined,false, false);
+  return executeQuery(getMyCurrentVerseRef(dcInstance, inputVars), inputOpts && { fetchPolicy: inputOpts.fetchPolicy });
+}
+
+export const getMyBookmarksRef = (dc) => {
+  const { dc: dcInstance} = validateArgs(connectorConfig, dc, undefined);
+  dcInstance._useGeneratedSdk();
+  return queryRef(dcInstance, 'GetMyBookmarks');
+}
+getMyBookmarksRef.operationName = 'GetMyBookmarks';
+
+export function getMyBookmarks(dcOrOptions, options) {
+  
+  const { dc: dcInstance, vars: inputVars, options: inputOpts } = validateArgsWithOptions(connectorConfig, dcOrOptions, options, undefined,false, false);
+  return executeQuery(getMyBookmarksRef(dcInstance, inputVars), inputOpts && { fetchPolicy: inputOpts.fetchPolicy });
+}
+
+export const getMyCompletionsRef = (dc) => {
+  const { dc: dcInstance} = validateArgs(connectorConfig, dc, undefined);
+  dcInstance._useGeneratedSdk();
+  return queryRef(dcInstance, 'GetMyCompletions');
+}
+getMyCompletionsRef.operationName = 'GetMyCompletions';
+
+export function getMyCompletions(dcOrOptions, options) {
+  
+  const { dc: dcInstance, vars: inputVars, options: inputOpts } = validateArgsWithOptions(connectorConfig, dcOrOptions, options, undefined,false, false);
+  return executeQuery(getMyCompletionsRef(dcInstance, inputVars), inputOpts && { fetchPolicy: inputOpts.fetchPolicy });
+}
 
--- dataconnect-generated/js/default-connector/index.d.ts
+++ dataconnect-generated/js/default-connector/index.d.ts
@@ -1,15 +1,211 @@
-import { ConnectorConfig, DataConnect } from 'firebase/data-connect';
+import { ConnectorConfig, DataConnect, QueryRef, QueryPromise, ExecuteQueryOptions, MutationRef, MutationPromise } from 'firebase/data-connect';
+
 export const connectorConfig: ConnectorConfig;
 
 export type TimestampString = string;
-
 export type UUIDString = string;
-
 export type Int64String = string;
-
 export type DateString = string;
 
 
 
 
+export interface AddBookmarkData {
+  bookmark_insert: Bookmark_Key;
+}
+
+export interface AddBookmarkVariables {
+  verseId: number;
+}
+
+export interface Bookmark_Key {
+  userId: string;
+  verseId: number;
+  __typename?: 'Bookmark_Key';
+}
+
+export interface CreateStudyCompletionData {
+  studyCompletion_insert: StudyCompletion_Key;
+}
+
+export interface CreateStudyCompletionVariables {
+  verseId: number;
+}
+
+export interface CreateUserData {
+  user_insert: User_Key;
+}
+
+export interface CreateUserVariables {
+  nickname: string;
+  termsAgreedAt: TimestampString;
+  privacyAgreedAt: TimestampString;
+  emailMarketingAgreed: boolean;
+  emailMarketingAgreedAt?: TimestampString | null;
+}
+
+export interface DeleteBookmarkData {
+  bookmark_delete?: Bookmark_Key | null;
+}
+
+export interface DeleteBookmarkVariables {
+  verseId: number;
+}
+
+export interface GetMyBookmarksData {
+  bookmarks: ({
+    verse: {
+      id: number;
+      reference: string;
+      fullText: string;
+    } & Verse_Key;
+      createdAt: TimestampString;
+  })[];
+}
+
+export interface GetMyCompletionsData {
+  studyCompletions: ({
+    id: UUIDString;
+    verse: {
+      id: number;
+      reference: string;
+    } & Verse_Key;
+      completedAt: TimestampString;
+  } & StudyCompletion_Key)[];
+}
+
+export interface GetMyCurrentVerseData {
+  studyCompletions: ({
+    verse: {
+      id: number;
+    } & Verse_Key;
+  })[];
+}
+
+export interface GetVerseData {
+  verse?: {
+    id: number;
+    reference: string;
+    fullText: string;
+    words: unknown;
+    maskedStep2: number[];
+    maskedStep3: number[];
+    maskedStep4: number[];
+  } & Verse_Key;
+}
+
+export interface GetVerseVariables {
+  id: number;
+}
+
+export interface StudyCompletion_Key {
+  id: UUIDString;
+  __typename?: 'StudyCompletion_Key';
+}
+
+export interface User_Key {
+  id: string;
+  __typename?: 'User_Key';
+}
+
+export interface Verse_Key {
+  id: number;
+  __typename?: 'Verse_Key';
+}
+
+interface CreateUserRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: CreateUserVariables): MutationRef<CreateUserData, CreateUserVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: CreateUserVariables): MutationRef<CreateUserData, CreateUserVariables>;
+  operationName: string;
+}
+export const createUserRef: CreateUserRef;
+
+export function createUser(vars: CreateUserVariables): MutationPromise<CreateUserData, CreateUserVariables>;
+export function createUser(dc: DataConnect, vars: CreateUserVariables): MutationPromise<CreateUserData, CreateUserVariables>;
+
+interface CreateStudyCompletionRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: CreateStudyCompletionVariables): MutationRef<CreateStudyCompletionData, CreateStudyCompletionVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: CreateStudyCompletionVariables): MutationRef<CreateStudyCompletionData, CreateStudyCompletionVariables>;
+  operationName: string;
+}
+export const createStudyCompletionRef: CreateStudyCompletionRef;
+
+export function createStudyCompletion(vars: CreateStudyCompletionVariables): MutationPromise<CreateStudyCompletionData, CreateStudyCompletionVariables>;
+export function createStudyCompletion(dc: DataConnect, vars: CreateStudyCompletionVariables): MutationPromise<CreateStudyCompletionData, CreateStudyCompletionVariables>;
+
+interface AddBookmarkRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: AddBookmarkVariables): MutationRef<AddBookmarkData, AddBookmarkVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: AddBookmarkVariables): MutationRef<AddBookmarkData, AddBookmarkVariables>;
+  operationName: string;
+}
+export const addBookmarkRef: AddBookmarkRef;
+
+export function addBookmark(vars: AddBookmarkVariables): MutationPromise<AddBookmarkData, AddBookmarkVariables>;
+export function addBookmark(dc: DataConnect, vars: AddBookmarkVariables): MutationPromise<AddBookmarkData, AddBookmarkVariables>;
+
+interface DeleteBookmarkRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: DeleteBookmarkVariables): MutationRef<DeleteBookmarkData, DeleteBookmarkVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: DeleteBookmarkVariables): MutationRef<DeleteBookmarkData, DeleteBookmarkVariables>;
+  operationName: string;
+}
+export const deleteBookmarkRef: DeleteBookmarkRef;
+
+export function deleteBookmark(vars: DeleteBookmarkVariables): MutationPromise<DeleteBookmarkData, DeleteBookmarkVariables>;
+export function deleteBookmark(dc: DataConnect, vars: DeleteBookmarkVariables): MutationPromise<DeleteBookmarkData, DeleteBookmarkVariables>;
+
+interface GetVerseRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (vars: GetVerseVariables): QueryRef<GetVerseData, GetVerseVariables>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect, vars: GetVerseVariables): QueryRef<GetVerseData, GetVerseVariables>;
+  operationName: string;
+}
+export const getVerseRef: GetVerseRef;
+
+export function getVerse(vars: GetVerseVariables, options?: ExecuteQueryOptions): QueryPromise<GetVerseData, GetVerseVariables>;
+export function getVerse(dc: DataConnect, vars: GetVerseVariables, options?: ExecuteQueryOptions): QueryPromise<GetVerseData, GetVerseVariables>;
+
+interface GetMyCurrentVerseRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (): QueryRef<GetMyCurrentVerseData, undefined>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect): QueryRef<GetMyCurrentVerseData, undefined>;
+  operationName: string;
+}
+export const getMyCurrentVerseRef: GetMyCurrentVerseRef;
+
+export function getMyCurrentVerse(options?: ExecuteQueryOptions): QueryPromise<GetMyCurrentVerseData, undefined>;
+export function getMyCurrentVerse(dc: DataConnect, options?: ExecuteQueryOptions): QueryPromise<GetMyCurrentVerseData, undefined>;
+
+interface GetMyBookmarksRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (): QueryRef<GetMyBookmarksData, undefined>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect): QueryRef<GetMyBookmarksData, undefined>;
+  operationName: string;
+}
+export const getMyBookmarksRef: GetMyBookmarksRef;
+
+export function getMyBookmarks(options?: ExecuteQueryOptions): QueryPromise<GetMyBookmarksData, undefined>;
+export function getMyBookmarks(dc: DataConnect, options?: ExecuteQueryOptions): QueryPromise<GetMyBookmarksData, undefined>;
+
+interface GetMyCompletionsRef {
+  /* Allow users to create refs without passing in DataConnect */
+  (): QueryRef<GetMyCompletionsData, undefined>;
+  /* Allow users to pass in custom DataConnect instances */
+  (dc: DataConnect): QueryRef<GetMyCompletionsData, undefined>;
+  operationName: string;
+}
+export const getMyCompletionsRef: GetMyCompletionsRef;
+
+export function getMyCompletions(options?: ExecuteQueryOptions): QueryPromise<GetMyCompletionsData, undefined>;
+export function getMyCompletions(dc: DataConnect, options?: ExecuteQueryOptions): QueryPromise<GetMyCompletionsData, undefined>;
 

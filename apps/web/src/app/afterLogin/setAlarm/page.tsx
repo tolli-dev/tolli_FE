a3b2805c4047fcf5f5356bb05524dacdeb5d7c9c
hjkim0905 <eax9952@gmail.com>
2026-06-01 16:58:21 +0900

    feat: add setAlarmTime UI

--- apps/web/src/app/afterLogin/setAlarm/page.tsx
+++ apps/web/src/app/afterLogin/setAlarm/page.tsx
@@ -22,17 +22,17 @@ export default function Page() {
     <section className="flex flex-col w-full flex-1 justify-between items-center px-[2.688rem] py-[clamp(2rem,5dvh,5.313rem)]">
       <div className="flex flex-col items-center w-full mt-[clamp(1rem,4dvh,10.25rem)]">
         <div className="flex flex-col items-center justify-center w-full gap-[0.563rem]">
-          <div className="flex flex-col items-center justify-center gap-[0.125rem]">
+          <div className="flex flex-col items-center justify-center gap-0.5">
             <h1 className="text-h1 text-primary-50">말씀이 찾아오는 시간,</h1>
             <h1 className="text-h1 text-primary-50">톨리가 알려드릴게요!</h1>
           </div>
-          <div className="flex flex-col items-center justify-center gap-[0.125rem]">
+          <div className="flex flex-col items-center justify-center gap-0.5">
             <h2 className="text-h2 text-surface-200">매일 정해진 시간에 조용히 알려드려요.</h2>
             <h2 className="text-h2 text-surface-200">tolli가 배고프면 찾아가요.</h2>
           </div>
         </div>
 
-        <div className="relative w-full max-w-[16.125rem] aspect-square">
+        <div className="relative w-full max-w-64.5 aspect-square">
           <Image src={TimeTolly} fill alt="timeTolli" className="object-contain" />
         </div>
       </div>
@@ -41,7 +41,7 @@ export default function Page() {
           type="button"
           onClick={requestAlarm}
           className="
-              w-full max-w-[19.688rem] h-[3rem] text-btn-lg 
+              w-full max-w-[19.688rem] h-12 text-btn-lg 
               text-bg bg-primary-75 rounded-[1.25rem]
             "
         >

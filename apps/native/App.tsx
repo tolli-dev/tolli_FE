9c711131e20865420da32b06a044b4640d9d6157
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-17 01:34:03 +0900

    refactor: edit IP_URL

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -21,8 +21,8 @@ import type {
 } from "react-native-webview";
 import * as Notifications from "expo-notifications";
 
-// const IP_URL = "https://tolli-fe-web.vercel.app/";
-const IP_URL = "http://192.168.35.166:3000";
+const IP_URL = "https://tolli-fe-web.vercel.app/";
+// const IP_URL = "http://192.168.35.166:3000";
 
 import {
   KakaoOAuthToken,

aeecfeebf4bef51ecda11ced55645146e24fe579
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-21 16:11:58 +0900

    refactor: add Login component (children pattern)

---
+++ apps/web/src/app/login/_components/Login.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Login({ children }: Props) {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center h-full">
+      {children}
+    </div>
+  );
+}
